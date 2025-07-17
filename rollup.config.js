d7b80d669f00dc658d6101f7fd12becb01ad4ee0
Mark Lai <marklai1998@pm.me>
2025-07-17 00:59:06 -0400

    chore: init project

---
+++ rollup.config.js
@@ -0,0 +1,57 @@
+import dts from "rollup-plugin-dts";
+import esbuild from "rollup-plugin-esbuild";
+import { nodeExternals } from "rollup-plugin-node-externals";
+import peerDepsExternal from "rollup-plugin-peer-deps-external";
+
+const input = {
+  index: "src/index.ts",
+  "vitest/index": "src/vitest/index.ts",
+};
+
+export default [
+  {
+    input,
+    plugins: [nodeExternals(), peerDepsExternal(), esbuild()],
+    output: [
+      {
+        dir: "dist/esm",
+        format: "es",
+        sourcemap: true,
+        entryFileNames: "[name].mjs",
+        chunkFileNames: "[name]-[hash].mjs",
+      },
+      {
+        dir: "dist/cjs",
+        format: "cjs",
+        interop: "auto",
+        sourcemap: true,
+        entryFileNames: "[name].cjs",
+        chunkFileNames: "[name]-[hash].cjs",
+      },
+    ],
+  },
+  {
+    input,
+    plugins: [
+      dts({
+        compilerOptions: {
+          // https://github.com/Swatinem/rollup-plugin-dts/issues/143
+          preserveSymlinks: false,
+        },
+      }),
+    ],
+    output: [
+      {
+        dir: "dist/esm",
+        format: "es",
+      },
+      {
+        dir: "dist/cjs",
+        format: "cjs",
+        interop: "auto",
+        entryFileNames: "[name].d.cts",
+        chunkFileNames: "[name]-[hash].d.cts",
+      },
+    ],
+  },
+];

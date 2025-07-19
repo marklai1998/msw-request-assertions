57b9895b5b8f834745502caffc03d4fb16df9057
Mark Lai <marklai1998@pm.me>
2025-07-18 20:21:57 -0400

    fix: type export

--- src/vitest/vitest.d.ts
+++ src/vitest/vitest.d.ts
@@ -33,5 +33,5 @@ interface CustomMatchers<R = unknown> {
 }
 
 declare module "vitest" {
-  interface Matchers<T = unknown> extends CustomMatchers<T> {}
+  interface Matchers<T = any> extends CustomMatchers<T> {}
 }

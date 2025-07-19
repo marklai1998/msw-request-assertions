687e3cfc4ef4975b41821dc49aa62741ea911d51
Mark Lai <marklai1998@pm.me>
2025-07-18 20:18:06 -0400

    fix: type export

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,5 @@
-import dts from "rollup-plugin-dts";
+import copy from "rollup-plugin-copy";
+import { dts } from "rollup-plugin-dts";
 import esbuild from "rollup-plugin-esbuild";
 import { nodeExternals } from "rollup-plugin-node-externals";
 import peerDepsExternal from "rollup-plugin-peer-deps-external";
@@ -12,7 +13,35 @@ const input = {
 export default [
   {
     input,
-    plugins: [nodeExternals(), peerDepsExternal(), esbuild()],
+    plugins: [
+      nodeExternals(),
+      peerDepsExternal(),
+      esbuild(),
+      copy({
+        targets: [
+          {
+            src: "src/vitest/vitest.d.ts",
+            dest: "dist/esm/vitest",
+            rename: "vitest.d.ts",
+          },
+          {
+            src: "src/vitest/vitest.d.ts",
+            dest: "dist/cjs/vitest",
+            rename: "vitest.d.cts",
+          },
+          {
+            src: "src/jest/jest.d.ts",
+            dest: "dist/esm/jest",
+            rename: "jest.d.ts",
+          },
+          {
+            src: "src/jest/jest.d.ts",
+            dest: "dist/cjs/jest",
+            rename: "jest.d.cts",
+          },
+        ],
+      }),
+    ],
     output: [
       {
         dir: "dist/esm",
@@ -28,23 +57,6 @@ export default [
         sourcemap: true,
         entryFileNames: "[name].cjs",
         chunkFileNames: "[name]-[hash].cjs",
-      },
-    ],
-  },
-  {
-    input,
-    plugins: [dts()],
-    output: [
-      {
-        dir: "dist/esm",
-        format: "es",
-      },
-      {
-        dir: "dist/cjs",
-        format: "cjs",
-        interop: "auto",
-        entryFileNames: "[name].d.cts",
-        chunkFileNames: "[name]-[hash].d.cts",
       },
     ],
   },
